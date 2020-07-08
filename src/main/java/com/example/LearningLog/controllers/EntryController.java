721759fc07853fe4d7f861f727f614c04e0b259f
DanilToLinus <danil-lomakin-02@mail.ru>
2020-07-08 17:48:14 +0500

    Another user page access protection.

--- src/main/java/com/example/LearningLog/controllers/EntryController.java
+++ src/main/java/com/example/LearningLog/controllers/EntryController.java
@@ -3,6 +3,7 @@ package com.example.LearningLog.controllers;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.LearningLog.models.Entry;
 import com.example.LearningLog.models.Topic;
+import com.example.LearningLog.models.User;
 import com.example.LearningLog.repos.EntryRepo;
 import com.example.LearningLog.repos.TopicRepo;
 
@@ -24,9 +26,15 @@ public class EntryController {
 
     @GetMapping("/topics/{topicId}/entries")
     public String entries(
-            @PathVariable(value="topicId") Integer topicId, Map<String, Object> model
+            @PathVariable(value="topicId") Integer topicId,
+            @AuthenticationPrincipal User current_user,
+            Map<String, Object> model
     ) {
         Topic topic = this.topicRepo.findById(topicId).get();
+        
+        if (!check_topic_owner(topic, current_user))
+            return "error_404";
+        
         model.put("topic", topic);
         
         Iterable<Entry> entries = this.entryRepo.findByTopicId(topicId);
@@ -37,16 +45,22 @@ public class EntryController {
     
     @GetMapping("/topics/{topicId}/entries/new_entry")
     public String new_entry(
-            @PathVariable(value="topicId") Integer topicId, Map<String, Object> model
+            @PathVariable(value="topicId") Integer topicId,
+            @AuthenticationPrincipal User current_user,
+            Map<String, Object> model
     ) {
         Topic topic = this.topicRepo.findById(topicId).get();
+        
+        if (!check_topic_owner(topic, current_user))
+            return "error_404";
+        
         model.put("topic", topic);
         
         return "new_entry";
     }
     
     @PostMapping("/topics/{topicId}/entries/new_entry")
-    public String addEntry(
+    public String add_entry(
             @PathVariable(value="topicId") Integer topicId,
             @RequestParam String text,
             Map<String, Object> model
@@ -71,5 +85,9 @@ public class EntryController {
          * 
          * return "redirect:/topics/" + topicId + "/entries";
          */
+    }
+    
+    public boolean check_topic_owner(Topic topic, User current_user) {
+        return topic.getOwner().getUsername().equals(current_user.getUsername());
     }
 }
