2ebff3b9aa2c89429d0b8b3124ba1255cb0d22a8
DanilToLinus <danil-lomakin-02@mail.ru>
2020-07-09 11:33:46 +0500

    Customization the 404 error page.

--- src/main/java/com/example/LearningLog/controllers/EntryController.java
+++ src/main/java/com/example/LearningLog/controllers/EntryController.java
@@ -3,12 +3,14 @@ package com.example.LearningLog.controllers;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.LearningLog.models.Entry;
 import com.example.LearningLog.models.Topic;
@@ -32,8 +34,7 @@ public class EntryController {
     ) {
         Topic topic = this.topicRepo.findById(topicId).get();
         
-        if (!check_topic_owner(topic, current_user))
-            return "error_404";
+        check_topic_owner(topic, current_user);
         
         model.put("topic", topic);
         
@@ -51,8 +52,7 @@ public class EntryController {
     ) {
         Topic topic = this.topicRepo.findById(topicId).get();
         
-        if (!check_topic_owner(topic, current_user))
-            return "error_404";
+        check_topic_owner(topic, current_user);
         
         model.put("topic", topic);
         
@@ -87,7 +87,12 @@ public class EntryController {
          */
     }
     
-    public boolean check_topic_owner(Topic topic, User current_user) {
-        return topic.getOwner().getUsername().equals(current_user.getUsername());
+    public void check_topic_owner(Topic topic, User current_user) {
+        /*** checks if the current user is the owner of the topic. ***/
+        
+        String topic_owner = topic.getOwner().getUsername();
+        
+        if (topic_owner.equals(current_user.getUsername()) != true)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
