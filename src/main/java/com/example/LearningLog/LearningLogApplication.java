afabdafd0deb28a2b73d383868008bb11207a0ca
DanilToLinus <danil-lomakin-02@mail.ru>
2020-08-06 16:10:59 +0500

    Removing the exces java code that includes the current user session into each template.

--- src/main/java/com/example/LearningLog/LearningLogApplication.java
+++ src/main/java/com/example/LearningLog/LearningLogApplication.java
@@ -9,12 +9,6 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 @SpringBootApplication
 public class LearningLogApplication {
     
-    public LearningLogApplication(FreeMarkerConfigurer freeMarkerConfigurer) {
-        freeMarkerConfigurer
-            .getTaglibFactory()
-            .setClasspathTlds(Collections.singletonList("/META-INF/security.tld"));
-    }
-    
 	public static void main(String[] args) {
 		SpringApplication.run(LearningLogApplication.class, args);
 	}
